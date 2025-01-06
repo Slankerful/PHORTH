478f0ba267b57c8868c4e5789764ba66529cf05c
slankerful <paulhorth@virginmedia.com>
2025-01-06 15:50:58 +0000

    Changes for see.fs

--- see.fs
+++ see.fs
@@ -1,48 +1,42 @@
-: END&START ( endof-word start-of-word )
-    PARSE-NAME SEARCH-WORD ( xt f -1 | xt f 1 )
-    ?DUP 0= IF -13 THROW THEN
-    ROT XT>LINK
-    HERE LATEST
-    BEGIN
-        2 PICK OVER <>
-    WHILE
-        NIP DUP @
-    REPEAT
-    DROP 4 - SWAP ;
-
 : MC? ( start-of-word -- flag )
     >CFA DUP @      ( cfa code-pointer )
     SWAP CELL+ =    ( does pointer pointer to next cell? )
+;
+
+: SEE-NAME 
+    XT>LINK LINK>NAME
+    .NAME DROP
 ;
 
 : SEE
     PARSE-NAME SEARCH-WORD ( c-addr u 0 | xt f -1 | xt f 1 )
     ?DUP 0= IF ERRWORD 2! -13 THROW THEN
     ROT         ( f -1 xt )
-    XT>LINK     ( f -1 link )
-    HERE LATEST ( f -1 link here latest ) ( start with end of last word and start of last word in dictionary )
-    BEGIN
-        2 PICK OVER     ( f -1 link here latest link latest )
-         <>             ( f -1 link here latest f2 )
-    WHILE
-        NIP DUP @       ( f -1 link latest prev )
-    REPEAT
-    DROP 4 - SWAP       ( f -1 end-of-word start-of-word )
-     DUP                 
-     LINK>NAME           ( f -1 end-of-word start-of-word nfa )
+    DUP 
+    @ VAR_DODOES =
+    IF ." VARIABLE " SEE-NAME 2DROP EXIT THEN
+    DUP @ 2VAR_DODOES =
+    IF ." 2VARIABLE " SEE-NAME 2DROP EXIT THEN
+    DUP @ CONST_DODOES =
+    IF ." CONSTANT " SEE-NAME 2DROP EXIT THEN
+    DUP @ 2CONST_DODOES =
+    IF ." 2CONSTANT " SEE-NAME 2DROP EXIT THEN
     [CHAR] : EMIT SPACE
-    .NAME DROP
-    DUP MC? DROP
-    LINK>NAME NAME>BODY
+    DUP
+    SEE-NAME
+    CELL+
+    DUP
     BEGIN
-         2DUP >
-     WHILE
-        DUP @ ( f -1 pfa cfa2 )
+         @    
+         DUP ['] EXIT <>
+    WHILE
+        DUP 
          CASE  
             ['] LIT OF
-                CELL+ DUP @ . ( Numeric literal )
+                DROP CELL+ DUP @ . ( Numeric literal )
             ENDOF
             ['] LITS OF
+                DROP
                 [CHAR] S EMIT [CHAR] " EMIT SPACE
                 CELL+ DUP @  ( Get length word )
                 SWAP CELL+ SWAP ( Get string address and set up for type )
@@ -52,24 +46,50 @@
                 4 -             ( Because of CELL+ later )
             ENDOF
             ['] 0BRANCH OF
+                DROP
                 ." 0BRANCH ( "
                 CELL+ DUP @
                 .
                 [CHAR] ) EMIT SPACE
             ENDOF
             ['] BRANCH OF
+                DROP
                 ." BRANCH ( "
                 CELL+ DUP @
                 .
                 [CHAR] ) EMIT SPACE
             ENDOF
-            ['] EXIT OF
+            ['] EXIT2 OF
+                DROP
                 ." EXIT "
             ENDOF
+            ['] (DO) OF
+                DROP
+                ." DO "
+            ENDOF
+            ['] (LOOP) OF
+                DROP
+                ." LOOP ( "
+                CELL+ DUP @
+                .
+                [CHAR] ) EMIT SPACE
+            ENDOF
+                ['] (+LOOP) OF
+                DROP
+                ." +LOOP ( "
+                CELL+ DUP @
+                .
+                [CHAR] ) EMIT SPACE
+            ENDOF
+                ['] (DOES>) OF
+                DROP
+                ." DOES> "
+                CELL+
+            ENDOF
          XT>LINK LINK>NAME .NAME DROP ( f -1 pfa )
-         DUP
        ENDCASE
        CELL+
+        DUP
     REPEAT
      2DROP
      [CHAR] ; EMIT 
